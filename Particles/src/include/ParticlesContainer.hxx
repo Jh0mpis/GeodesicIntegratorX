dffce68a3e69d043449c016518c4522153c0cafc
Jh0mpis <jmorenotr@unal.edu.co>
2025-08-14 18:19:11 +0200

    add: Add the Particle thorn header files for the ParticlesContainer abstract and concrete classes and the Particles structs definitions

---
+++ Particles/src/include/ParticlesContainer.hxx
@@ -0,0 +1,85 @@
+/**
+ * @file ParticlesContainer.hxx
+ *
+ * @brief Contains the Iterator and Particles namespaces.
+ *
+ * This file contains the definition of the Iterator and Particles namespaces.
+ *
+ * The Iterator namespace contains the definition to a convenient class to
+ * iterate over the particles: the ParticleIterator class. The Particles
+ * namespace defines the different <Particles>Container classes, where
+ * <Particle> depend on the equations to solve. This is the concrete class that
+ * implements the abstract methods defined on the abstract
+ * BaseParticlesContainer class.
+ */
+
+#ifndef PARTICLESCONTAINER_H
+#define PARTICLESCONTAINER_H
+
+// Import libraries
+#include "BaseParticlesContainer.hxx"
+#include <AMReX_AmrParticles.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_MultiFabUtil.H>
+#include <AMReX_Particles.H>
+#include <AMReX_REAL.H>
+#include <cctk_Arguments.h>
+
+static constexpr const char* INFO = "INFO (GeodesicsIntegratorX/Particles): ";
+
+namespace Iterator {
+
+template <typename StructType>
+class ParticleIterator
+    : public amrex::ParIter<0, 0, StructType::n_attributes, 0> {
+public:
+  using Base = amrex::ParIter<0, 0, StructType::n_attributes, 0>;
+  using Base::ParIter;
+  using RealVector = typename amrex::ParIter<
+      0, 0, StructType::n_attributes>::ContainerType::RealVector;
+
+  const std::array<RealVector, StructType::n_attributes> &GetAttribs() const {
+    return this->GetStructOfArrays().GetRealData();
+  }
+
+  std::array<RealVector, StructType::n_attributes> &GetAttribs() {
+    return this->GetStructOfArrays().GetRealData();
+  }
+
+  const RealVector &GetAttribs(int comp) const {
+    return this->GetStructOfArrays().GetRealData(comp);
+  }
+
+  RealVector &GetAttribs(int comp) {
+    return this->GetStructOfArrays().GetRealData(comp);
+  }
+}; // class ParicleIterator
+
+} // namespace Iterator
+
+namespace Containers {
+
+template <typename StructType>
+class PhotonsContainer
+    : public BaseParticlesContainer::BaseParticlesContainer<StructType> {
+
+public:
+  PhotonsContainer(amrex::AmrCore *amr_core)
+      : BaseParticlesContainer::BaseParticlesContainer<StructType>(amr_core) {}
+
+  void initialize() override {
+    std::cout<<INFO<<"Initializing Particles with "<<StructType::n_attributes<<" attributes."<<std::endl;
+  }
+
+  void evolve() override {
+    std::cout<<INFO<<"Evolving Particles"<<std::endl;
+  }
+};
+
+template <typename StructType>
+class MassiveContainer
+    : public BaseParticlesContainer::BaseParticlesContainer<StructType> {};
+
+} // namespace Containers
+
+#endif // !PARTICLESCONTAINER_H
